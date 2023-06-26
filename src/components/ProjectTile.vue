1fbfdfafde81f2e3ac7633867c12d29ef7b54e0c
unknown <blazejjosko@hotmail.de>
2023-06-26 21:47:29 +0200

    added first projects

---
+++ src/components/ProjectTile.vue
@@ -0,0 +1,123 @@
+<template>
+    <div class="project">
+        <h1>{{ title  }}</h1>
+        <div>{{ desc }}</div>
+        <a class="ref" :href="link">VIEW ON GITHUB</a>
+        <div class="tags">
+            <span
+                class="tag"
+                v-for="tag in tags" 
+                :key="tag">
+                {{ tag }}
+            </span>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'ProjectTile',
+    props: {
+        title: {
+            type: String,
+            default: 'Project',    
+        },
+        desc: {
+            type: String,
+            default: 'Description'
+        },
+        link: {
+            type: String,
+            default: ''
+        },
+        tags: {
+            type: Array,
+            default: () => []
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.project {
+    height: 300px;
+    width: 400px;
+    max-width: 100vw;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    position: relative;
+    align-items: center;
+
+    .ref {
+        width: 150px;
+        background-color: $primary-color;
+        border-radius: 10px;
+        height: 50px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: $quad-color;
+        font-weight: bold;
+    }
+
+    .tags {
+        display: flex;
+        width: 400px;
+        max-width: 100vw;
+        justify-content: space-evenly;
+        gap: 10px;
+        flex-wrap: wrap;
+        
+        .tag {
+            background-color: $tertiary-color;
+            border-radius: 5px;
+            width: unset;
+            padding: 3px;
+            bottom: 0px;
+        }
+    }
+}
+
+@media only screen and (min-width: 1200px) {
+    .project {
+    height: 300px;
+    width: 500px;
+    border: 1px solid salmon;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    position: relative;
+    align-items: center;
+
+    .ref {
+        width: 150px;
+        background-color: $primary-color;
+        border-radius: 10px;
+        height: 50px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: $quad-color;
+        font-weight: bold;
+    }
+
+    .tags {
+        display: flex;
+        width: 500px;
+        justify-content: space-evenly;
+        gap: 10px;
+        flex-wrap: wrap;
+        
+        .tag {
+            background-color: $tertiary-color;
+            border-radius: 5px;
+            width: unset;
+            padding: 3px;
+            bottom: 0px;
+        }
+    }
+}
+}
+</style>
---
+++ src/components/SoftwareProfile.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="softwareprofile">
+        <h1 id="softwareCategory" class="category">Software</h1>
+        <ProjectTile
+            title="TS3-Monitoring Bot"
+            desc="A bot which reacts to specified commands send through teamspeak to allow for a simple monitoring."
+            link="https://github.com/Borsche/teamspeak-monitoring-bot"
+            :tags="['Node.js', 'Typescript', 'API', 'Teamspeak3']"
+        />
+        <ProjectTile
+            title="Twitch Voting Tool"
+            desc="A tool for twitch which allows the streamer to set up quick Polls. The viewers can then vote through the chat. Trough customizable shortcuts the Streamer can work more efficiently."
+            link="https://github.com/Borsche/teamspeak-monitoring-bot"
+            :tags="['Node.js', 'Typescript', 'Electron', 'API', 'Twitch']"
+        />
+        <ProjectTile
+            title="Loyalty Twitch"
+            desc="A huge WIP Project. A software which allows the viewers to earn points by watching the stream and playing minigames with others. These points can then be spend on by the streamer defined commands. The old Java Codebase is currently being redone in Node.js."
+            link="https://github.com/Borsche/teamspeak-monitoring-bot"
+            :tags="['Node.js', 'Typescript', 'PostgreSQL', 'API', 'Twitch', 'Java', 'SpringBoot', 'Vue.js']"
+        />
+    </div>
+</template>
+
+<script>
+import ProjectTile from './ProjectTile.vue'
+
+export default {
+    name: 'SoftwareProfile',
+    components: {
+        ProjectTile,
+    }
+}
+</script>
+
+<style lang="scss">
+.softwareprofile {
+    background-color: $quad-color;
+    scroll-snap-align: start;
+
+    display: flex;
+    flex-wrap: wrap;
+    align-content: space-around;
+    flex-direction: row;
+    justify-content: space-evenly;
+
+    .category {
+        width: 100vw;
+    }
+}
+
+@media only screen and (min-width: 1200px) {
+    .statblock {
+
+        .stat {
+            font-size: 8rem;
+        }
+
+        .desc {
+            color: $tertiary-color;
+            font-size: 2rem;
+        }
+    }
+}
+
+</style>
